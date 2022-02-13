a0feae54caa8000cc682c55c0b0e02ecac17a52f
AdinHal <80127944+AdinHal@users.noreply.github.com>
2022-02-13 20:34:15 +0100

    Created and logged all face detections on sample image with face-api.js

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import * as faceapi from "face-api.js"
+import './App.css';
+import {useEffect, useRef} from "react";
+
+function App() {
+    const imgRef = useRef()
+    const canvasRef = useRef()
+
+    const handleImage = async()=>{
+        const detections = await faceapi.detectAllFaces(imgRef.current, new faceapi.TinyFaceDetectorOptions()
+        ).withFaceLandmarks()
+            .withFaceExpressions()
+        console.log(detections)
+    }
+
+    useEffect(()=>{
+        const loadModels = ()=>{
+            Promise.all([
+                faceapi.nets.tinyFaceDetector.loadFromUri("/models"),
+                faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+                faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+                faceapi.nets.faceExpressionNet.loadFromUri("/models")
+            ]).then(handleImage)
+                .catch((e)=>{console.log(e)})
+        }
+
+        imgRef.current && loadModels()
+    },[])
+  return (
+    <div className="App">
+        <canvas ref={canvasRef} width="900" height="550"/>
+      <img crossOrigin="anonymous" ref={imgRef} src="https://n-cdn.serienjunkies.de/hq/106927.jpg" alt="" width={"900"} height={"550"}/>
+    </div>
+  );
+}
+
+export default App;
